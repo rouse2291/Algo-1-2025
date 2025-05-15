97da3b5a5aa3eecb00ffee75b8e587ceac2b8767
Rosa Salas <202400996c@utea.edu.pe>
2025-05-15 11:50:22 -0500

    listo

---
+++ Colores.java
@@ -0,0 +1,11 @@
+public class Colores {
+
+    public static void main(String[] args) {
+       
+        System.out.println("\033[30m mandarina");
+        System.out.println("[32m verde");
+        System.out.println("[31m 3. tomate");
+        System.out.println("[36m 4. cielo");
+        System.out.println("[34m 5. mar");
+    }
+}
---
+++ HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        //muestra en pntalla la frase hola mundo 
+        System.out.println("Hola mundo!!!");
+    }
+} 
