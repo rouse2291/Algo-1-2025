e9bead3a6527bb4cce9e1b8fa73e8c54832fdfb5
Rosa Salas <202400996c@utea.edu.pe>
2025-05-19 09:45:21 -0500

    tarea

---
+++ Ejercicios1/Ejercicio4.java
@@ -0,0 +1,20 @@
+package Ejercicios1;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        System.out.println("_________________________________________________");
+        System.out.println("| Lunes | Martes | Miercoles | Jueves | Viernes |");
+        System.out.println("_________________________________________________");
+        System.out.println("|Computer  |\tOrdenador  |");
+        System.out.println("|Student   |\tAlumno/a   |");
+        System.out.println("|Cat       |\tGato       |");
+        System.out.println("|Penguin   |\tPingüino   |");
+        System.out.println("|Machine   |\tMaquina    |");
+        System.out.println("|Nature    |\tNaturaleza |");
+        System.out.println("|Light     |\tLuz        |");
+        System.out.println("|Greem     |\tVerde      |");
+        System.out.println("|Book      |\tLibro      |");
+        System.out.println("|Pyramind  |\tPiramide   |");
+        System.out.println("|__________|_______________|");
+    }
+}
