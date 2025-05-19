c5e96f97816ae2f1bbf03763bd399b375c4b8762
Rosa Salas <202400996c@utea.edu.pe>
2025-05-19 09:37:58 -0500

    tarea

--- Ejercicios1/Ejercicio3.java
+++ Ejercicios1/Ejercicio3.java
@@ -2,8 +2,19 @@ package Ejercicios1;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        System.out.println("|  Ingles  |  Español  |");
-        System.out.println("________________________");
-        System.out.println("________________________");
+        System.out.println("___________________________");
+        System.out.println("|  Ingles  |    Español    |");
+        System.out.println("___________________________");
+        System.out.println("|Computer  |\tOrdenador  |");
+        System.out.println("|Student   |\tAlumno/a   |");
+        System.out.println("|Cat       |\tGato       |");
+        System.out.println("|Penguin   |\tPingüino   |");
+        System.out.println("|Machine   |\tMaquina    |");
+        System.out.println("|Nature    |\tNaturaleza |");
+        System.out.println("|Light     |\tLuz        |");
+        System.out.println("|Greem     |\tVerde      |");
+        System.out.println("|Book      |\tLibro      |");
+        System.out.println("|Pyramind  |\tPiramide   |");
+        System.out.println("|__________|_______________|");
     }
 }
