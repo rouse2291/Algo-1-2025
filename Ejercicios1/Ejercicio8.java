498843caf950e602b5defd7e3c0fca9bbf1c5f15
Rosa Salas <202400996c@utea.edu.pe>
2025-05-22 10:39:03 -0500

    tarea

---
+++ Ejercicios1/Ejercicio8.java
@@ -0,0 +1,10 @@
+package Ejercicios1;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println("//\\     //\\ ");
+        System.out.println("{   `---'  }  ");
+        System.out.println("{    O O   }  ");
+        System.out.println(" ~~~> V <~~~  ");
+    }
+}
