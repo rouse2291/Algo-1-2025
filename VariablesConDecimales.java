457aa49107bb21969fcb342a69ebeedc27635385
Rosa Salas <202400996c@utea.edu.pe>
2025-05-26 09:09:28 -0500

    wi

---
+++ VariablesConDecimales.java
@@ -0,0 +1,13 @@
+public class VariablesConDecimales {
+    public static void main(String[] args) {
+        double x;
+        double y;
+
+        x =7;
+        y=25.0023;
+
+        System.out.println("x vale: "+ x);
+        System.out.println("y vale: "+ y);
+    }
+    
+}
---
+++ VariablesEnteras.java
@@ -0,0 +1,12 @@
+public class VariablesEnteras {
+    public static void main(String[] args) {
+        int x;
+        x=5;
+
+        System.out.println("El valor actual de mi variable es: "+ x);
+
+        x=7;
+
+        System.out.println("Y ahora es: " + x);
+    }    
+}
