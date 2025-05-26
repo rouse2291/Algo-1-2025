5add844c4787e9caaa8befc09b3ab8ae77b9962d
Rosa Salas <202400996c@utea.edu.pe>
2025-05-26 09:34:13 -0500

    ti

---
+++ UsoDeChar.java
@@ -0,0 +1,16 @@
+public class UsoDeChar {
+    public static void main(String[] args) {
+        char letra1 = 'c';
+        char letra2 = 'a';
+        char letra3 = 's';
+        char letra4 = 'a';
+
+        System.out.println("Letra 1: "+ letra1);
+        System.out.println("Letra 2: "+ letra2);
+        System.out.println("Letra 3: "+ letra3);
+        System.out.println("Letra 4: "+ letra4);
+        System.out.println("Todas juntas: "+ letra1+letra2+letra3+letra4);
+    
+    }
+    
+}
---
+++ UsoDeString.java
@@ -0,0 +1,10 @@
+public class UsoDeString {
+    public static void main(String[] args) {
+        String miPalabra ="Cerveza";
+        String miFrase = "¿Donde esta mi cerveza?";
+    
+        System.out.println("Una palabra que uso con frecuencia: "+ miPalabra);
+        System.out.println("Una frase que uso a veces: "+ miFrase);
+    }
+    
+}
