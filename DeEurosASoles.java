196915db78c56a805aaa0e82035f984666af1b11
Rosa Salas <202400996c@utea.edu.pe>
2025-06-02 09:44:28 -0500

    clase

--- DeEurosASoles.java
+++ DeEurosASoles.java
@@ -1,14 +1,14 @@
 public class DeEurosASoles {
     public static void main(String[] args) {
-                String linea;
+        String linea;
 
         System.out.println("Ingrese la canidad de soles: ");
 
         linea = System.console().readLine();
         double numero1;
-        numero1= Integer.parseInt(linea);
+        numero1= Double.parseDouble(linea);
 
-        System.out.println("La cantidad de soles a euros: "+(numero1/4));
+        System.out.println("La cantidad de soles a euros: "+(numero1/4.20));
     }
     
 }
---
+++ LeerDatosScanner.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LeerDatosScanner {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+
+       //System.out.println("Ingrese su nombre: ");
+       //String nombre = s.nextLine();
+       //System.out.println("Ingrese su edad: ");
+       //int edad = Integer.parseInt(s.nextLine());
+
+       System.out.println("Ingrese su nombre y edad separado po un espacio: ");
+       String nombre = s.next();
+       int edad = s.nextInt();
+
+
+        System.out.println("Tu  nombre es "+nombre+" y tu edad es "+edad);
+
+    }
+    
+}
