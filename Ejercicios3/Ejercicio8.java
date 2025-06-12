00f456d0ca07987a2ae3e7830305bcf08411b071
Rosa Salas <202400996c@utea.edu.pe>
2025-06-12 11:55:42 -0500

    h

--- Ejercicios3/Ejercicio8.java
+++ Ejercicios3/Ejercicio8.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args) {
         Scanner Teclado = new Scanner(System.in);
-
+        double num1,resultado;
         
+        System.out.println("Ingrese la cantidad de Mb: ");
+        num1 = Teclado.nextDouble();
+        resultado = num1 /1000;
+        System.out.println("La cantidad de Mb a Kb: "+resultado);
 
     }
 }
---
+++ Ejercicios3/Ejercicio9.java
@@ -0,0 +1,10 @@
+package Ejercicios3;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args) {
+        Scanner Teclado = new Scanner(System.in);
+        
+    }
+}
