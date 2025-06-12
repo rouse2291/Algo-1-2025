2bd22d5935a19449a288977380098cad2f0ae727
Rosa Salas <202400996c@utea.edu.pe>
2025-06-12 11:25:11 -0500

    tjl

--- Ejercicios3/Ejercicio3.java
+++ Ejercicios3/Ejercicio3.java
@@ -3,7 +3,16 @@ package Ejercicios3;
 import java.util.Scanner;
 
 public class Ejercicio3 {
-    Scanner Teclado = new Scanner(System.in);
-    
+    public static void main(String[] args) {
+        Scanner Teclado = new Scanner(System.in);
+
+        double num1,resultado;
+
+        System.out.println("Ingrese la cantidad de pesetas: ");
+
+        num1 = Teclado.nextDouble();
+        resultado = num1 /0.000053110;
+        System.out.println("La cantidad de pesetas a euros es: "+resultado);
+    }
     
 }
