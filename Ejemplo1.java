ad673249df0b632c9f7e4ddeb380997ebc588bef
Rosa Salas <202400996c@utea.edu.pe>
2025-06-16 09:15:22 -0500

    hl

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class Ejemplo1 {
     public static void main(String[] args) {
         
-        double respuesta;
+        double prac,exp,inv,exa,act;
         Scanner Teclado = new Scanner(System.in);
 
-        System.out.println("Ingresa tu promedio");
-        respuesta = Teclado.nextDouble();
+        System.out.println("Ingresa las 5 notas separadas por un espacio");
 
-        if (respuesta>=10.5) {
+        prac = Teclado.nextDouble();
+        exp = Teclado.nextDouble();
+        inv = Teclado.nextDouble();
+        exa = Teclado.nextDouble();
+        act = Teclado.nextDouble();
+        
+        double promedio = (prac+exp+inv+exa+act)/5;
+
+        if (promedio>=10.5) {
             System.out.println("Usted tiene su promedio aprobado");
             
         } else {
--- Ejercicios3/Ejercicio9.java
+++ Ejercicios3/Ejercicio9.java
@@ -6,7 +6,16 @@ public class Ejercicio9 {
     public static void main(String[] args) {
         Scanner Teclado = new Scanner(System.in);
 
-        double num1,resultado;
+        double nota1,nota2,necesario,deseo;
+        System.out.println("Ingresa la primera nota");
+        nota1 = Teclado.nextDouble();
+
+        System.out.println("Que nota desea sacar en el examen");
+        deseo = Teclado.nextDouble();
+
+        nota2= (deseo-(nota1*0.4))/0.6;
+
+        System.out.println("Para tener "+deseo+"en el examen debes sacar:"+nota2);
         
 
 
