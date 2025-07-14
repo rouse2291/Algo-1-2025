79aa92773aca682ae4fa33e9d64d0802aaba8046
Rosa Salas <202400996c@utea.edu.pe>
2025-07-14 09:38:30 -0500

    AH

---
+++ Ejercicios6/Ejercicio1.java
@@ -0,0 +1,77 @@
+package Ejercicios6;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Baraja francesa al azar");
+
+        int baraja=(int)(Math.random() * 3);
+        int numeros=(int)(Math.random() * 12);
+
+        switch (baraja) {
+            case 0:
+            System.out.println("corazon");
+                
+                break;
+            case 1:
+            System.out.println("diamantes");
+                
+                break;
+            case 2:
+            System.out.println("treboles");
+                
+                break;
+            case 3:
+            System.out.println("picas");
+                
+                break;       
+            default:
+                break;
+        }
+        switch (numeros) {
+            case 0:
+                System.out.println("A");
+                break;
+            case 1:
+                System.out.println("K");
+                break;
+            case 2:
+                System.out.println("Q");
+                break;
+            case 3:
+                System.out.println("J");
+                break;
+            case 4:
+                System.out.println("11");
+                break;
+            case 5:
+                System.out.println("10");
+                break;
+            case 6:
+                System.out.println("9");
+                break;
+            case 7:
+                System.out.println("8");
+                break;
+            case 8:
+                System.out.println("7");
+                break;
+            case 9:
+                System.out.println("6");
+                break;
+            case 10:
+                System.out.println("5");
+                break;
+            case 11:
+                System.out.println("4");
+                break;
+            case 12:
+                System.out.println("4");
+                break;
+                
+            default:
+                break;
+        }
+
+    }
+    
+}
