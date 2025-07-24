41645ebc2363b86b6655b755316e739be90925e4
Rosa Salas <202400996c@utea.edu.pe>
2025-07-24 11:04:16 -0500

    si

--- EjArrays2.java
+++ EjArrays2.java
@@ -1,37 +1,27 @@
 import java.util.Scanner;
 
 public class EjArrays2 {
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-
-        int[] numero = new int [20];
-        int[] cuadrado = new int [20];
-        int[] cubo = new int [20];
-
-        for (int i = 0; i <=19; i++) {
-             numero[i] = (int)(Math.random()*101);
-            
-        }
-        for (int i = 0; i <=19; i++) {
-            cuadrado[i] = numero [i] * numero[i];
-            
-        }
-        for (int i = 0; i <=19; i++) {
-            cubo[i] = numero[i] * numero [i] * numero[i];
-        }
-        for (int i = 0; i < numero.length; i++) {
-            System.out.println("El array de numeros tiene un posicion: "+(i+1));
-            System.out.println(numero[i]);
-            
-        }
-        for (int i = 0; i < numero.length; i++) {
-             System.out.println("El array de cuadrado tiene un posicion: "+(i+1));
-            System.out.println(cuadrado[i]);
-        }
-        for (int i = 0; i < numero.length; i++) {
-             System.out.println("El array de cubos tiene un posicion: "+(i+1));
-            System.out.println(cubo[i]);
-        }
-    }
+      public static void main(String[] args) {
     
+        int[] numero = new int[20];
+        int[] cuadrado = new int[20];
+        int[] cubo = new int[20];
+
+        int i;
+
+        for (i = 0; i < 20; i++) {
+            numero[i] = (int) (Math.random() * 101);
+            cuadrado[i] = numero[i] * numero[i];
+            cubo[i] = cuadrado[i] * numero[i];
+        }
+
+        System.out.println(
+                "\nA continuación se muestran en tres columnas, un número aleatorio entre 0 y 100, su cuadrado y su cubo:\n");
+
+        System.out.println("  n  │   n²  │    n³\n─────┼───────┼─────────");
+        for (i = 0; i < 20; i++) {
+            System.out.printf("%4d │ %5d │%8d\n", numero[i], cuadrado[i], cubo[i]);
+        }
+
+    }
 }
